5b2ff56d05ba545b7f90f89e8f6748e1aaef8763
rodrigo laime <rodrigolaime141@gmail.com>
2023-02-17 12:17:37 -0300

    first commit

---
+++ src/routes/App.jsx
@@ -0,0 +1,30 @@
+/* import { useState } from 'react' */
+/* import reactLogo from '../assets/react.svg' */
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import React from 'react'
+import './App.css'
+import { Menu } from './Menu'
+import { Home } from './home/Home'
+import { About } from './about/About'
+import { Jobs } from './jobs/Jobs'
+import { Contact } from './contact/Contact'
+
+
+function App() {
+  /* const [count, setCount] = useState(0) */
+
+  return (
+    <HashRouter className="App">
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/jobs' element={<Jobs />} />
+        <Route path='/contact' element={<Contact />} />
+        {/* <Route path='*' element={<p>Not Found</p>} /> */}
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
---
+++ src/routes/Menu.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Menu.css'
+const Menu = () => {
+  return (
+    <nav className='menu-nav'>
+      <ul className='menu-ul'>
+        {routes.map(route => (
+          <li className='menu-li'>
+            <NavLink
+              className='a'
+              key={routes.text}
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.to}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+const routes = [];
+routes.push({
+  to: '/',
+  text: 'Home'
+})
+routes.push({
+  to: '/about',
+  text: 'About'
+})
+routes.push({
+  to: '/jobs',
+  text: 'Jobs'
+})
+routes.push({
+  to: '/contact',
+  text: 'Contact'
+})
+
+export { Menu }
