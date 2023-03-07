dc6319ce2d48232a98c3ef40aa4663588f5b27aa
Rodrigo Laime <rodrigolaime141@gmail.com>
2023-03-07 20:37:09 -0300

    scroll failed

--- src/routes/menu/Menu.jsx
+++ src/routes/menu/Menu.jsx
@@ -27,7 +27,7 @@ const Menu = () => {
   const [scrollnav, setScrollnav] = useState(false);
 
   const changeScroll = () => {
-    /* console.log(window.scrollY) */
+    console.log(window.scrollY)
     if(window.screenY >=80){
       setScrollnav(true)
     } else {
@@ -45,11 +45,11 @@ const Menu = () => {
     const menu1 = document.getElementById('menu1')
     menu1.classList.toggle('openmenu1');
   }
-
+/* <nav className={scrollnav ? 'menu-nav scrNav' : 'menu-nav'}> */
   return (
     <div className='container-nav'>
-      <nav className='menu-nav '>
-        <h4 className='menu-h4'> <span> Portafolio</span> | Rodrigo</h4>
+      <nav className='menu-nav'>
+        <h4 className='menu-h4'> <span> Portfolio</span> | Rodrigo</h4>
 
         <div className='container-icon-menu' onClick={handleClick}>
           <div onClick={click} className='menu1' id='menu1'>
